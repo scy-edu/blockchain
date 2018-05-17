02fbcf92799bf0f0049963a9f89740666b1e9cbd
Stanley Yang <me@stanleycyang.com>
2018-05-17 11:53:21 -0700

    Step 1: Created the blueprint for our blockchain

---
+++ blockchain.py
@@ -0,0 +1,41 @@
+'''
+What our Block will look like:
+
+block = {
+    'index': 1,
+    'timestamp': 1506057125.900785,
+    'transactions': [
+        {
+            'sender': "8527147fe1f5426f9dd545de4b27ee00",
+            'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+            'amount': 5,
+        }
+    ],
+    'proof': 324984774000,
+    'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+}
+
+'''
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+    
+    def new_block(self):
+        # Creates a new Block and add it to the Blockchain
+        pass
+
+    def new_transactions(self):
+        # Adds a new transaction to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        pass
+
+    @property
+    def last_block(self):
+        # Return the last Block in the chain
+        pass
