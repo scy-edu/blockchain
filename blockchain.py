d3465b6118745ba5295aa6188a052cb15eb8b36c
Stanley Yang <me@stanleycyang.com>
2018-05-17 12:36:13 -0700

    Step 4: Added Flask

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+
+from flask import Flask, jsonify, request
 
 '''
 What our Block will look like:
@@ -76,7 +80,7 @@ class Blockchain(object):
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
     
-    def new_block(self):
+    def new_block(self, proof, previous_hash=None):
         """
         Create a new Block on our Blockchain
 
@@ -134,3 +138,31 @@ class Blockchain(object):
     def last_block(self):
         # Return the last Block in the chain
         return self.chain[-1]
+
+# Create our node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "Mine"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "New Transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
